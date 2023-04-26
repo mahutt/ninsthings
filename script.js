77b837376957c519b8cd4331c0803edce1dcf353
mahutt <tommy.mahut@gmail.com>
2023-04-26 00:20:47 -0400

    size select buttons work

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function changeImage(direction) {
-    index = document.getElementById("imageIndex");
-    index.innerHTML = ((index.innerHTML + direction) % 3);
-    image = document.getElementById("product-image");
-    image.src = image.src.replace(/\d+\.jpg$/, index.innerHTML + ".jpg");
-    // console.log(index.innerHTML);
+function selected(label) {
+    const labels = label.parentNode.querySelectorAll('label');
+    for (var i = 0; i < labels.length; i++) {
+        if (labels[i].classList.contains("selected-size"))
+            labels[i].classList.remove("selected-size");
+    }
+    label.classList.add("selected-size");     
 }
+
+// APPLYING THE selected() FUNCTION TO ALL LABELS
+const sizeLabels = document.querySelectorAll(".size-label");
+for (var i = 0; i < sizeLabels.length; i++) {
+    sizeLabels[i].addEventListener('click', function() {
+        selected(this);
+    });
+}
+
